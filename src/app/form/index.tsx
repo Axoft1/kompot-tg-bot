2f266d1c2a39f8b1bb2a9bf66df10d679db24478
Aleksei Anufriev <axoft10@gmail.com>
2024-01-29 12:05:26 +0700

    added functionality to the form

--- src/app/form/index.tsx
+++ src/app/form/index.tsx
@@ -1,15 +1,39 @@
-import React, { memo } from "react"
+import React, { memo, useEffect, useState } from "react"
 import './style.scss'
+import { useTelegram } from "../../hooks/useTelegram"
 function Form() {
+  const { tg } = useTelegram()
+  const [city, setCity] = useState('')
+  const [address, setAddress] = useState('')
+
+  const onChangeCity = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.target.value)
+  }
+  const onChangeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value)
+  }
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Отправить'
+    })
+  }, [tg.MainButton])
+  useEffect(() => {
+    if (!city || !address) {
+      tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
+    }
+  }, [])
   return (
     <div className="form">
       <span>Город</span>
-      <select className="select">
+      <select className="select" value={city} onChange={onChangeCity}>
         <option value="moscow">Москва</option>
         <option value="kazan">Казань</option>
       </select>
       <span>Улица</span>
-      <input className="input" type="text" placeholder="Улица" />
+      <input className="input" value={address} onChange={onChangeAddress} type="text" placeholder="Улица" />
     </div>
   )
 }
