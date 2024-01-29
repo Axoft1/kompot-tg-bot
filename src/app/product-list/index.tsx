4d310d3fc9525cb8924b93b6bab5c7eee81b618a
Aleksei Anufriev <axoft10@gmail.com>
2024-01-29 11:10:52 +0700

    style

--- src/app/product-list/index.tsx
+++ src/app/product-list/index.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from 'react'
-
+import './style.scss'
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className='productList'>ProductList!</div>
   )
 }
 
